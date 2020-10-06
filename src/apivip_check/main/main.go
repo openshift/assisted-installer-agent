1bc57a6bf2f9e91746e0f982112d67e7a4f43768
Daniel Erez <danielerez@gmail.com>
2020-10-06 18:06:05 +0300

    MGMT-2075 - API VIP connectivity check command (#38)
    * Added apivip_check command:
    - For ensuring connectivity between the node to and the API.
    - Done by invoking a GET request to <api>/config/worker
      (to download the worker.ign file).
    
    * Added relevant tests with wiremock stubs in subsystem/apivip_check_test.

---
+++ src/apivip_check/main/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/openshift/assisted-installer-agent/src/config"
+	"github.com/openshift/assisted-installer-agent/src/util"
+
+	"github.com/openshift/assisted-installer-agent/src/apivip_check"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	config.ProcessSubprocessArgs(false, true)
+	util.SetLogging("apivip_check", config.SubprocessConfig.TextLogging, config.SubprocessConfig.JournalLogging)
+	if flag.NArg() != 1 {
+		log.Warnf("Expecting exactly single argument to apivip_check. Received %d", len(os.Args)-1)
+		os.Exit(-1)
+	}
+	stdout, stderr, exitCode := apivip_check.CheckAPIConnectivity(flag.Arg(0), log.StandardLogger())
+	fmt.Fprint(os.Stdout, stdout)
+	fmt.Fprint(os.Stderr, stderr)
+	os.Exit(exitCode)
+}
---
+++ src/apivip_check/utils.go
@@ -0,0 +1,33 @@
+package apivip_check
+
+import (
+	"bytes"
+	"text/template"
+)
+
+const nodeIgnitionFormat = `{
+	"ignition": {
+		"version": "3.1.0",
+		"config": {
+			"merge": [{
+				"source": "{{.SOURCE}}"
+			}]
+		}
+	}
+}`
+
+func FormatNodeIgnitionFile(source string) ([]byte, error) {
+	var ignitionParams = map[string]string{
+		"SOURCE": source,
+	}
+	
+	tmpl, err := template.New("nodeIgnition").Parse(nodeIgnitionFormat)
+	if err != nil {
+		return nil, err
+	}
+	buf := &bytes.Buffer{}
+	if err = tmpl.Execute(buf, ignitionParams); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}

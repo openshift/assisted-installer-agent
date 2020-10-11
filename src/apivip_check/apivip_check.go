11c372356b59a0ddb086e4831b6526c898d47ad7
Daniel Erez <derez@redhat.com>
2020-10-11 13:38:51 +0300

    MGMT-2318 network CIDR verification to API VIP (#46)
    Added network CIDR verification to API VIP connectivity check:
    - Parse IP from API VIP.
    - Get interfaces of the node.
    - Calculate CIDRs by interfaces.
    - API is verified if contained in one of the CIDRs.

--- src/apivip_check/apivip_check.go
+++ src/apivip_check/apivip_check.go
@@ -3,7 +3,9 @@ package apivip_check
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"net/url"
 
 	"github.com/openshift/assisted-service/models"
 	"github.com/pkg/errors"
@@ -29,10 +31,18 @@ func CheckAPIConnectivity(checkAPIRequestStr string, log logrus.FieldLogger) (st
 		return createRepsonse(false), err.Error(), -1
 	}
 	
-	if err := httpDownload(*checkAPIRequest.URL + WorkerIgnitionPath, log); err != nil {
+	if err := httpDownload(*checkAPIRequest.URL + WorkerIgnitionPath); err != nil {
 		wrapped := errors.Wrap(err, "Failed to download worker.ign file")
 		log.WithError(err).Error(wrapped.Error())
 		return createRepsonse(false), wrapped.Error(), -1
+	}
+
+	if checkAPIRequest.VerifyCidr {
+		if err := verifyCIDR(*checkAPIRequest.URL); err != nil {
+			wrapped := errors.Wrap(err, "CheckAPIConnectivity: failure verifying CIDR of API VIP")
+			log.WithError(err).Error(wrapped.Error())
+			return createRepsonse(false), wrapped.Error(), -1
+		}
 	}
 
 	return createRepsonse(true), "", 0
@@ -49,7 +59,7 @@ func createRepsonse(success bool) string {
 	return string(bytes)
 }
 
-func httpDownload(uri string, log logrus.FieldLogger) error {
+func httpDownload(uri string) error {
     res, err := http.Get(uri)
     if err != nil {
 		return errors.Wrap(err, "HTTP download failure")
@@ -65,10 +75,64 @@ func httpDownload(uri string, log logrus.FieldLogger) error {
 		return errors.New("Empty Ignition file")
 	}
 
-	var js json.RawMessage
+    var js json.RawMessage
     if err = json.Unmarshal(bytes, &js); err != nil {
 		return errors.Wrap(err, "Error unmarshaling Ignition string")
 	}
 
     return err
+}
+
+func verifyCIDR(uri string) error {
+	apiVip, err := getIPByURI(uri)
+	if err != nil {
+		return errors.Wrap(err, "Failed to get VIP API")
+	}
+
+	_, err = calculateMachineNetworkCIDR(apiVip)
+	if err != nil {
+		return errors.Wrap(err, "Failed to calculate network CIDR")
+	}
+
+	return nil
+}
+
+func getIPByURI(uri string) (net.IP, error) {
+	u, err := url.Parse(uri)
+    if err != nil {
+		return nil, errors.Wrap(err, "Failed parsing specified URL")
+	}
+	
+	host, _, _ := net.SplitHostPort(u.Host)
+	if ip := net.ParseIP(host); ip != nil {
+		return ip, nil
+	}
+
+	addr, err := net.LookupIP(host)
+	if err != nil {
+	   return nil, errors.Wrap(err, "Unknown host for specified API VIP")
+	}
+	return addr[0], nil
+}
+
+func calculateMachineNetworkCIDR(apiVip net.IP) (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", errors.Wrap(err, "Failed to fetch machine's interfaces")
+	}
+
+	for _, intf := range interfaces {
+		addrs, _ := intf.Addrs()
+		for _, ipv4addr := range addrs {
+			_, ipnet, err := net.ParseCIDR(ipv4addr.String())
+			if err != nil {
+				continue
+			}
+			if ipnet.Contains(apiVip) {
+				return ipnet.String(), nil
+			}
+		}
+	}
+
+	return "", errors.Errorf("No suitable matching CIDR found for API VIP %s", apiVip)
 }
--- src/apivip_check/apivip_check_test.go
+++ src/apivip_check/apivip_check_test.go
@@ -25,39 +25,68 @@ var _ = Describe("API connectivity check test", func() {
 		srv.Close()
 	})
 
-	It("HTTP download ignition file", func() {
-		srv = serverMock(ignitionMock)
-		_, _, exitCode := CheckAPIConnectivity(getRequestStr(&srv.URL), log)
-		Expect(exitCode).Should(Equal(0))
+	Context("Ignition file", func() {
+		It("Download ignition file successfully", func() {
+			srv = serverMock(ignitionMock)
+			_, _, exitCode := CheckAPIConnectivity(getRequestStr(&srv.URL, false), log)
+			Expect(exitCode).Should(Equal(0))
+		})
+
+		It("Invalid ignition file format", func() {
+			srv = serverMock(ignitionMockInvalid)
+			_, _, exitCode := CheckAPIConnectivity(getRequestStr(&srv.URL, false), log)
+			Expect(exitCode).Should(Equal(-1))
+		})
+
+		It("Empty ignition", func() {
+			srv = serverMock(ignitionMockEmpty)
+			_, _, exitCode := CheckAPIConnectivity(getRequestStr(&srv.URL, false), log)
+			Expect(exitCode).Should(Equal(-1))
+		})
 	})
 
-	It("Invalid ignition file format", func() {
-		srv = serverMock(ignitionMockInvalid)
-		_, _, exitCode := CheckAPIConnectivity(getRequestStr(&srv.URL), log)
-		Expect(exitCode).Should(Equal(-1))
+	Context("API URL", func() {
+		It("Invalid API URL", func() {
+			url := "http://127.0.0.1:2345"
+			_, _, exitCode := CheckAPIConnectivity(getRequestStr(&url, false), log)
+			Expect(exitCode).Should(Equal(-1))
+		})
+
+		It("Missing API URL", func() {
+			_, _, exitCode := CheckAPIConnectivity(getRequestStr(nil, false), log)
+			Expect(exitCode).Should(Equal(-1))
+		})
 	})
 
-	It("Empty ignition", func() {
-		srv = serverMock(ignitionMockEmpty)
-		_, _, exitCode := CheckAPIConnectivity(getRequestStr(&srv.URL), log)
-		Expect(exitCode).Should(Equal(-1))
-	})
+	Context("Verify CIDR", func() {
+		It("Verification success - hostname", func() {
+			srv = serverMock(ignitionMock)
+			err := verifyCIDR("http://localhost:1234")
+			Expect(err).ToNot(HaveOccurred())
+		})
 
-	It("Invalid API URL", func() {
-		url := "http://127.0.0.1:2345"
-		_, _, exitCode := CheckAPIConnectivity(getRequestStr(&url), log)
-		Expect(exitCode).Should(Equal(-1))
-	})
+		It("Verification success - IP address", func() {
+			srv = serverMock(ignitionMock)
+			err := verifyCIDR("http://127.0.0.1:1234")
+			Expect(err).ToNot(HaveOccurred())
+		})
 
-	It("Missing API URL", func() {
-		_, _, exitCode := CheckAPIConnectivity(getRequestStr(nil), log)
-		Expect(exitCode).Should(Equal(-1))
+		It("Invalid URL", func() {
+			err := verifyCIDR("http://invalid:1234")
+			Expect(err).To(HaveOccurred())
+		})
+
+		It("CIDR not suitable", func() {
+			err := verifyCIDR("http://example.com:1234")
+			Expect(err).To(HaveOccurred())
+		})
 	})
 })
 
-func getRequestStr(url *string) string {
+func getRequestStr(url *string, verifyCidr bool) string {
 	request := models.APIVipConnectivityRequest{
 		URL: url,
+		VerifyCidr: verifyCidr,
 	}
 
 	requestBytes, err := json.Marshal(request)

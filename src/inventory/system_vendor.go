c3194394a014cfaaaa7ab585f5fb7b833822914f
Omer Tuchfeld <omertuchfeld@gmail.com>
2020-11-18 14:30:07 +0200

    MGMT-792 - Updated assisted-service, replaced lshw with ghw, populated is-vm field (#89)
    * MGMT-792 - Updated assisted-service, replaced lshw with ghw, populated is-vm field
    
    * MGMT-792 Removed lshw from dockerfile because it's no longer in use

--- src/inventory/system_vendor.go
+++ src/inventory/system_vendor.go
@@ -1,33 +1,42 @@
 package inventory
 
 import (
-	"encoding/json"
-
+	"github.com/jaypipes/ghw"
 	"github.com/openshift/assisted-service/models"
 	"github.com/sirupsen/logrus"
+
+	"strings"
 )
 
-type systemVendor struct {
-	Product string
-	Serial  string
-	Vendor  string
+func isVirtual(product string) bool {
+	for _, vmTech := range []string {
+		"KVM",
+		"VirtualBox",
+		"VMware",
+		"Virtual Machine",
+	} {
+		if strings.Contains(product, vmTech) {
+			return true
+		}
+	}
+
+	return false
 }
 
 func GetVendor(dependencies IDependencies) *models.SystemVendor {
-	o, e, exitCode := dependencies.Execute("lshw", "-quiet", "-json")
 	var ret models.SystemVendor
-	if exitCode != 0 {
-		logrus.Errorf("Error running lshw:: %s", e)
-		return &ret
-	}
-	var sv systemVendor
-	err := json.Unmarshal([]byte(o), &sv)
+
+	product, err := dependencies.Product(ghw.WithChroot("/host"))
+
 	if err != nil {
-		logrus.WithError(err).Errorf("Error unmarshaling %s", o)
+		logrus.Errorf("Error running ghw.Product with /host chroot:: %s", err)
 		return &ret
 	}
-	ret.SerialNumber = sv.Serial
-	ret.ProductName = sv.Product
-	ret.Manufacturer = sv.Vendor
+
+	ret.SerialNumber = product.SerialNumber
+	ret.ProductName = product.Name
+	ret.Manufacturer = product.Vendor
+	ret.Virtual = isVirtual(product.Name)
+
 	return &ret
 }
--- src/inventory/system_vendor_test.go
+++ src/inventory/system_vendor_test.go
@@ -1,130 +1,11 @@
 package inventory
 
 import (
+	"fmt"
+	"github.com/jaypipes/ghw"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/openshift/assisted-service/models"
-)
-
-const (
-	lshwOutput = `{
-    "id": "ibm-p8-05-fsp.mgmt.pnr.lab.eng.rdu2.redhat.com",
-    "class": "system",
-    "claimed": true,
-    "handle": "DMI:0012",
-    "description": "Notebook",
-    "product": "20NYS7K91V (LENOVO_MT_20NY_BU_Think_FM_ThinkPad T490s)",
-    "vendor": "LENOVO",
-    "version": "ThinkPad T490s",
-    "serial": "PC1E81XA",
-    "width": 64,
-    "configuration": {
-        "administrator_password": "disabled",
-        "chassis": "notebook",
-        "family": "ThinkPad T490s",
-        "power-on_password": "disabled",
-        "sku": "LENOVO_MT_20NY_BU_Think_FM_ThinkPad T490s",
-        "uuid": "CC5D820B-6430-B211-A85C-968EC80B7FCF"
-    },
-    "capabilities": {
-        "smbios-3.1.1": "SMBIOS version 3.1.1",
-        "dmi-3.1.1": "DMI version 3.1.1",
-        "smp": "Symmetric Multi-Processing",
-        "vsyscall32": "32-bit processes"
-    },
-    "children": [
-        {
-            "id": "core",
-            "class": "bus",
-            "claimed": true,
-            "handle": "DMI:0013",
-            "description": "Motherboard",
-            "product": "20NYS7K91V",
-            "vendor": "LENOVO",
-            "physid": "0",
-            "version": "Not Defined",
-            "serial": "L1HF02A02SD",
-            "slot": "Not Available",
-            "children": [
-                {
-                    "id": "battery",
-                    "class": "power",
-                    "claimed": true,
-                    "handle": "DMI:002C",
-                    "product": "02DL014",
-                    "vendor": "SMP",
-                    "physid": "1",
-                    "slot": "Front",
-                    "units": "mWh",
-                    "capacity": 57020,
-                    "configuration": {
-                        "voltage": "11.5V"
-                    }
-                }
-            ]
-        }
-    ]
-}
-`
-	malformedLshwOutput = `{
-    "id": "ibm-p8-05-fsp.mgmt.pnr.lab.eng.rdu2.redhat.com",
-    "class": "system",
-    "claimed": true,
-    "handle": "DMI:0012",
-    "description": "Notebook",
-    "product": "20NYS7K91V (LENOVO_MT_20NY_BU_Think_FM_ThinkPad T490s)",
-    "vendor": "LENOVO",
-    "version": "ThinkPad T490s",
-    "serial": "PC1E81XA",
-    "width": 64,
-    "configuration": yyy {
-        "administrator_password": "disabled",
-        "chassis": "notebook",
-        "family": "ThinkPad T490s",
-        "power-on_password": "disabled",
-        "sku": "LENOVO_MT_20NY_BU_Think_FM_ThinkPad T490s",
-        "uuid": "CC5D820B-6430-B211-A85C-968EC80B7FCF"
-    },
-    "capabilities": {
-        "smbios-3.1.1": "SMBIOS version 3.1.1",
-        "dmi-3.1.1": "DMI version 3.1.1",
-        "smp": "Symmetric Multi-Processing",
-        "vsyscall32": "32-bit processes"
-    },
-    "children": [
-        {
-            "id": "core",
-            "class": "bus",
-            "claimed": true,
-            "handle": "DMI:0013",
-            "description": "Motherboard",
-            "product": "20NYS7K91V",
-            "vendor": "LENOVO",
-            "physid": "0",
-            "version": "Not Defined",
-            "serial": "L1HF02A02SD",
-            "slot": "Not Available",
-            "children": [
-                {
-                    "id": "battery",
-                    "class": "power",
-                    "claimed": true,
-                    "handle": "DMI:002C",
-                    "product": "02DL014",
-                    "vendor": "SMP",
-                    "physid": "1",
-                    "slot": "Front",
-                    "units": "mWh",
-                    "capacity": 57020,
-                    "configuration": {
-                        "voltage": "11.5V"
-                    }
-                }
-            ]
-        }
-    ]
-}
-`
 )
 
 var _ = Describe("System vendor test", func() {
@@ -138,23 +19,37 @@ var _ = Describe("System vendor test", func() {
 		dependencies.AssertExpectations(GinkgoT())
 	})
 
-	It("Execute error", func() {
-		dependencies.On("Execute", "lshw", "-quiet", "-json").Return(lshwOutput, "Execute error", -1)
+	It("Product error", func() {
+		dependencies.On("Product", ghw.WithChroot("/host")).Return(nil, fmt.Errorf("Just an error")).Once()
 		ret := GetVendor(dependencies)
 		Expect(ret).To(Equal(&models.SystemVendor{}))
 	})
-	It("Json error", func() {
-		dependencies.On("Execute", "lshw", "-quiet", "-json").Return(malformedLshwOutput, "", 0)
-		ret := GetVendor(dependencies)
-		Expect(ret).To(Equal(&models.SystemVendor{}))
-	})
-	It("lshw OK", func() {
-		dependencies.On("Execute", "lshw", "-quiet", "-json").Return(lshwOutput, "", 0)
+	It("Product OK", func() {
+		dependencies.On("Product", ghw.WithChroot("/host")).Return(&ghw.ProductInfo{
+			Name:         "A Name",
+			SerialNumber: "A Serial Number",
+			Vendor:       "A Vendor",
+		}, nil).Once()
+
 		ret := GetVendor(dependencies)
 		Expect(ret).To(Equal(&models.SystemVendor{
-			Manufacturer: "LENOVO",
-			ProductName:  "20NYS7K91V (LENOVO_MT_20NY_BU_Think_FM_ThinkPad T490s)",
-			SerialNumber: "PC1E81XA",
+			ProductName:  "A Name",
+			SerialNumber: "A Serial Number",
+			Manufacturer: "A Vendor",
 		}))
+	})
+	It("Virtual machine detection", func() {
+		for _, test := range []struct {
+			Product string
+			IsVm    bool
+		}{
+			{"KVM", true},
+			{"VirtualBox ()", true},
+			{"VMware Virtual Platform ()", true},
+			{"Virtual Machine", true},
+			{"20T1S39D3N (LENOVO_MT_20T1_BU_Think_FM_ThinkPad T14s Gen 1)", false},
+		} {
+			Expect(isVirtual(test.Product)).Should(Equal(test.IsVm))
+		}
 	})
 })

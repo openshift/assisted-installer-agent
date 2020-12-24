01f177e54bcc7ed9a8526ffe2b4f978a83c893c4
Omer Tuchfeld <otuchfel@redhat.com>
2020-12-24 13:13:51 +0200

    OCPBUGSM-22486 - Assisted installer agent will now consider AHV to be a virtual machine

--- src/inventory/system_vendor_test.go
+++ src/inventory/system_vendor_test.go
@@ -47,6 +47,7 @@ var _ = Describe("System vendor test", func() {
 			{"VirtualBox ()", true},
 			{"VMware Virtual Platform ()", true},
 			{"Virtual Machine", true},
+			{"AHV", true},
 			{"20T1S39D3N (LENOVO_MT_20T1_BU_Think_FM_ThinkPad T14s Gen 1)", false},
 		} {
 			Expect(isVirtual(test.Product)).Should(Equal(test.IsVm))

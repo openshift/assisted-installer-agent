04cfff799c0adf5d8cb2b9485d0717b411a80d61
Fred Rolland <frolland@redhat.com>
2021-01-03 18:00:24 +0200

    MGMT-3458 Send step reply from FIO perf check

--- src/fio_perf_check/fio_perf_check.go
+++ src/fio_perf_check/fio_perf_check.go
@@ -1,10 +1,18 @@
 package fio_perf_check
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
+
+	"github.com/openshift/assisted-installer-agent/src/config"
+	"github.com/openshift/assisted-installer-agent/src/session"
+	"github.com/openshift/assisted-service/client/installer"
 	"github.com/openshift/assisted-service/models"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -39,18 +47,53 @@ func (p *PerfCheck) FioPerfCheck(fioPerfCheckRequestStr string, log logrus.Field
 		return createResponse(-1), err.Error(), -1
 	}
 
-	log.Infof("FIO result on disk %s :fdatasync duration %d ms , threshold: %d ms", *fioPerfCheckRequest.Path, diskPerf, *fioPerfCheckRequest.DurationThreshold)
+	log.Infof("FIO result on disk %s :fdatasync duration %d ms , threshold: %d ms", *fioPerfCheckRequest.Path, diskPerf, *fioPerfCheckRequest.DurationThresholdMs)
 
-	if diskPerf > *fioPerfCheckRequest.DurationThreshold {
+	response := createResponse(diskPerf)
+
+	invSession, err := session.New(config.GlobalAgentConfig.TargetURL, config.GlobalAgentConfig.PullSecretToken)
+	if err != nil {
+		log.Fatalf("Failed to initialize connection: %e", err)
+	} else {
+		params := installer.PostStepReplyParams{
+			HostID:                strfmt.UUID(config.GlobalAgentConfig.HostID),
+			ClusterID:             strfmt.UUID(config.GlobalAgentConfig.ClusterID),
+			DiscoveryAgentVersion: &config.GlobalAgentConfig.AgentVersion,
+		}
+		reply := models.StepReply{
+			StepType: models.StepTypeFioPerfCheck,
+			StepID:   "fio-perf-check",
+			ExitCode: 0,
+			Output:   response,
+			Error:    "",
+		}
+
+		params.Reply = &reply
+		_, err = invSession.Client().Installer.PostStepReply(context.Background(), &params)
+		if err != nil {
+			switch errValue := err.(type) {
+			case *installer.PostStepReplyInternalServerError:
+				log.Warnf("Assisted service returned status code %s after processing step reply. Reason: %s", http.StatusText(http.StatusInternalServerError), swag.StringValue(errValue.Payload.Reason))
+			case *installer.PostStepReplyUnauthorized:
+				log.Warn("User is not authenticated to perform the operation")
+			case *installer.PostStepReplyBadRequest:
+				log.Warnf("Assisted service returned status code %s after processing step reply.  Reason: %s", http.StatusText(http.StatusBadRequest), swag.StringValue(errValue.Payload.Reason))
+			default:
+				log.WithError(err).Warn("Error posting step reply")
+			}
+		}
+	}
+
+	if diskPerf > *fioPerfCheckRequest.DurationThresholdMs {
 		// If the 99th percentile of fdatasync durations is more than 10ms, it's not fast enough for etcd.
 		// See: https://www.ibm.com/cloud/blog/using-fio-to-tell-whether-your-storage-is-fast-enough-for-etcd
-		log.WithError(err).Errorf("Disk %s is not fast enough for installation (fdatasync duration: %d)", 
+		log.WithError(err).Errorf("Disk %s is not fast enough for installation (fdatasync duration: %d)",
 			*fioPerfCheckRequest.Path, diskPerf)
-		return createResponse(diskPerf), fmt.Sprintf("Disk %s is not fast enough for installation",
+		return response, fmt.Sprintf("Disk %s is not fast enough for installation",
 			*fioPerfCheckRequest.Path), int(*fioPerfCheckRequest.ExitCode)
 	}
 
-	return createResponse(diskPerf), "", 0
+	return response, "", 0
 }
 
 // Returns the 99th percentile of fdatasync durations in milliseconds
@@ -72,7 +115,7 @@ func (p *PerfCheck) getDiskPerf(path string) (int64, error) {
 			Sync struct {
 				LatNs struct {
 					Percentile struct {
-						Nine9_000000  int64 `json:"99.000000"`
+						Nine9_000000 int64 `json:"99.000000"`
 					} `json:"percentile"`
 				} `json:"lat_ns"`
 			} `json:"sync"`

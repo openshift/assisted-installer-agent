1cfbb1162ff1abd64d70ebb9a16e92f4f2c08075
Jordi Gil <jgil@redhat.com>
2021-04-27 10:31:50 -0400

    MGMT-4718 Capture network latency for L3

--- src/commands/connectivity_check.go
+++ src/commands/connectivity_check.go
@@ -5,17 +5,16 @@ import (
 	"encoding/xml"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/openshift/assisted-installer-agent/src/util"
 	"github.com/openshift/assisted-installer-agent/src/util/nmap"
 	"github.com/openshift/assisted-service/models"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
-
-type Done struct{}
-
-type Any interface{}
 
 func getOutgoingNics() []string {
 	ret := make([]string, 0)
@@ -55,94 +54,225 @@ func getIPAddressFromCIDR(cidr string) string {
 	return ""
 }
 
-func sendDone(ch chan Any) {
-	ch <- Done{}
+type any interface{}
+
+type done struct{}
+
+func sendDone(ch chan any) {
+	ch <- done{}
 }
 
-func l3CheckAddressOnNic(address string, outgoingNic string, l3chan chan *models.L3Connectivity) {
+const pingCount string = "10"
+
+func l3CheckAddressOnNic(address string, outgoingNic string, innerChan chan *models.L3Connectivity, conCheck connectivityCmd) {
 	ret := &models.L3Connectivity{
 		OutgoingNic:     outgoingNic,
 		RemoteIPAddress: address,
-		Successful:      false,
 	}
-	cmd := exec.Command("ping", "-c", "2", "-W", "3", "-I", outgoingNic, address)
-	_, err := cmd.CombinedOutput()
+	b, err := conCheck.command("ping", []string{"-c", pingCount, "-W", "3", "-q", "-I", outgoingNic, address})
 	if err != nil {
-		log.Infof("Error running ping to %s on interface %s: %s", address, outgoingNic, err.Error())
-		ret.Successful = false
-	} else {
-		ret.Successful = true
+		log.Errorf("Error running ping to %s on interface %s: %s", address, outgoingNic, err.Error())
+		innerChan <- ret
+		return
 	}
-	l3chan <- ret
+	err = parsePingCmd(ret, string(b))
+	if err != nil {
+		log.Error(err)
+		innerChan <- ret
+		return
+	}
+	ret.Successful = true
+	innerChan <- ret
 }
 
-func l3CheckAddress(address string, outgoingNics []string, l3chan, doneChan chan Any) {
-	defer sendDone(doneChan)
-	innerChan := make(chan *models.L3Connectivity, 1000)
+func regexMatchFor(regex, line string) ([]string, error) {
+	r := regexp.MustCompile(regex)
+	p := r.FindStringSubmatch(line)
+	if len(p) < 2 {
+		return nil, errors.Errorf("unable to parse %s with regex %s", line, regex)
+	}
+	return p, nil
+}
+
+func parsePingCmd(conn *models.L3Connectivity, cmdOutput string) error {
+	if len(cmdOutput) == 0 {
+		return errors.Errorf("Missing output for ping or invalid output:\n%s", cmdOutput)
+	}
+	parts, err := regexMatchFor(`[\d]+ packets transmitted, [\d]+ received, (([\d]*[.])?[\d]+)% packet loss, time [\d]+ms`, cmdOutput)
+	if err != nil {
+		return errors.Errorf("Unable to retrieve packet loss percentage: %s", err)
+	}
+	conn.PacketLossPercentage, err = strconv.ParseFloat(parts[1], 64)
+	if err != nil {
+		return errors.Errorf("Error while trying to convert value for packet loss '%s': %s", parts[1], err)
+	}
+	parts, err = regexMatchFor(`rtt min\/avg\/max\/mdev = .*\/([^\/]+)\/.*\/.* ms`, cmdOutput)
+	if err != nil {
+		return errors.Errorf("Unable to retrieve the average RTT for ping: %s", err)
+	}
+	conn.AverageRTTMs, err = strconv.ParseFloat(parts[1], 64)
+	if err != nil {
+		return errors.Errorf("Error while trying to convert value for packet loss %s: %s", parts[1], err)
+	}
+	return nil
+}
+
+func l3CheckConnectivity(addresses []string, dataCh chan any, conCheck connectivityCmd) {
+
+	defer sendDone(dataCh)
+	wg := sync.WaitGroup{}
+	wg.Add(len(addresses))
+	for _, address := range addresses {
+		go l3CheckAddress(address, conCheck.getOutgoingNICs(), dataCh, &wg, conCheck)
+	}
+	wg.Wait()
+}
+
+func l3CheckAddress(address string, outgoingNics []string, dataCh chan any, wg *sync.WaitGroup, conCheck connectivityCmd) {
+	defer wg.Done()
+	innerChan := make(chan *models.L3Connectivity)
 	for _, nic := range outgoingNics {
-		go l3CheckAddressOnNic(address, nic, innerChan)
+		go l3CheckAddressOnNic(address, nic, innerChan, conCheck)
 	}
 	successful := false
 	for i := 0; i != len(outgoingNics); i++ {
 		ret := <-innerChan
 		if ret.Successful {
-			l3chan <- ret
+			dataCh <- ret
 			successful = true
 		}
 	}
 	if !successful {
 		ret := &models.L3Connectivity{
 			RemoteIPAddress: address,
-			Successful:      false,
 		}
-		l3chan <- ret
+		dataCh <- ret
 	}
 }
 
-func l3CheckConnectivity(addresses []string, outgoingNics []string, l3chan chan Any) {
-	defer sendDone(l3chan)
-	doneChan := make(chan Any)
-	for _, address := range addresses {
-		go l3CheckAddress(address, outgoingNics, l3chan, doneChan)
-	}
-	for i := 0; i != len(addresses); i++ {
-		<-doneChan
-	}
-}
-
-func macInDstMacs(mac string, allDstMacs []string) bool {
-	for _, dstMac := range allDstMacs {
-		if strings.EqualFold(mac, dstMac) {
+func macInDstMacs(mac string, allDstMACs []string) bool {
+	for _, dstMAC := range allDstMACs {
+		if strings.EqualFold(mac, dstMAC) {
 			return true
 		}
 	}
 	return false
 }
 
-func l2CheckAddressOnNic(dstAddr string, dstMac string, allDstMacs []string, srcNic string, l2chan chan Any) {
-	defer sendDone(l2chan)
+func l2CheckAddressOnNic(dstAddr string, dstMAC string, allDstMACs []string, srcNIC string, dataCh chan any, conCheck connectivityCmd) {
+	defer sendDone(dataCh)
 	if util.IsIPv4Addr(dstAddr) {
-		runArping(dstAddr, dstMac, allDstMacs, srcNic, l2chan)
+		l2IPv4Cmd(dstAddr, dstMAC, allDstMACs, srcNIC, dataCh, conCheck)
 	} else {
-		cmd := exec.Command("nmap", "-6", "-sn", "-n", "-oX", "-", "-e", srcNic, dstAddr)
-		analyzeNmap(dstAddr, dstMac, allDstMacs, srcNic, l2chan, cmd.Output)
+		analyzeNmap(dstAddr, dstMAC, allDstMACs, srcNIC, dataCh, conCheck)
+	}
+
+}
+
+func analyzeNmap(dstAddr string, dstMAC string, allDstMACs []string, srcNIC string, dataCh chan any, conCheck connectivityCmd) {
+
+	ret := &models.L2Connectivity{
+		OutgoingNic:     srcNIC,
+		RemoteIPAddress: dstAddr,
+	}
+
+	out, err := conCheck.command("nmap", []string{"-6", "-sn", "-n", "-oX", "-", "-e", srcNIC, dstAddr})
+	if err != nil {
+		log.WithError(err).Warn("nmap command failed")
+		dataCh <- ret
+		return
+	}
+
+	var nmaprun nmap.Nmaprun
+	if err := xml.Unmarshal(out, &nmaprun); err != nil {
+		log.WithError(err).Warn("Failed to un-marshal nmap XML")
+		dataCh <- ret
+		return
+	}
+
+	for _, h := range nmaprun.Hosts {
+		if h.Status.State != "up" {
+			continue
+		}
+		for _, a := range h.Addresses {
+			if a.AddrType != "mac" {
+				continue
+			}
+			remoteMAC := strings.ToLower(a.Addr)
+			ret.RemoteMac = remoteMAC
+			ret.Successful = macInDstMacs(remoteMAC, allDstMACs)
+			if !ret.Successful {
+				log.Warnf("Unexpected MAC address for nmap %s on NIC %s: %s", dstAddr, srcNIC, remoteMAC)
+			} else if strings.ToLower(dstMAC) != remoteMAC {
+				log.Infof("Received remote MAC %s different then expected MAC %s", remoteMAC, dstMAC)
+			}
+
+			dataCh <- ret
+			return
+		}
+	}
+	dataCh <- ret
+}
+
+func l2CheckAddress(dstAddr string, dstMAC string, allDstMACs []string, dataCh chan any, wg *sync.WaitGroup, conCheck connectivityCmd) {
+	defer wg.Done()
+	innerChan := make(chan any)
+	for _, srcNIC := range conCheck.getOutgoingNICs() {
+		go l2CheckAddressOnNic(dstAddr, dstMAC, allDstMACs, srcNIC, innerChan, conCheck)
+	}
+	received := false
+	for numDone := 0; numDone != len(conCheck.getOutgoingNICs()); {
+		iret := <-innerChan
+		switch ret := iret.(type) {
+		case *models.L2Connectivity:
+			received = true
+			dataCh <- ret
+		case done:
+			numDone++
+		}
+	}
+	if !received {
+		dataCh <- &models.L2Connectivity{
+			RemoteIPAddress: dstAddr,
+		}
 	}
 }
 
-func runArping(dstAddr string, dstMac string, allDstMacs []string, srcNic string, l2chan chan Any) {
-
-	ret := &models.L2Connectivity{
-		OutgoingNic:     srcNic,
-		RemoteIPAddress: dstAddr,
-		RemoteMac:       "",
-		Successful:      false,
+func l2CheckConnectivity(dataCh chan any, conCheck connectivityCmd) {
+	defer sendDone(dataCh)
+	allDstMACs := make([]string, len(conCheck.getHost().Nics))
+	for i, destNic := range conCheck.getHost().Nics {
+		allDstMACs[i] = destNic.Mac
 	}
-	cmd := exec.Command("arping", "-c", "1", "-w", "2", "-I", srcNic, dstAddr)
-	bytes, _ := cmd.CombinedOutput()
+	numAddresses := 0
+	for _, destNic := range conCheck.getHost().Nics {
+		numAddresses += len(destNic.IPAddresses)
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(numAddresses)
+	for _, destNic := range conCheck.getHost().Nics {
+		for _, address := range destNic.IPAddresses {
+			go l2CheckAddress(address, destNic.Mac, allDstMACs, dataCh, &wg, conCheck)
+		}
+	}
+	wg.Wait()
+}
+
+func l2IPv4Cmd(dstAddr string, dstMAC string, allDstMACs []string, srcNIC string, dataCh chan any, conCheck connectivityCmd) {
+	ret := &models.L2Connectivity{
+		OutgoingNic:     srcNIC,
+		RemoteIPAddress: dstAddr,
+	}
+	bytes, err := conCheck.command("arping", []string{"-c", "1", "-w", "2", "-I", srcNIC, dstAddr})
+	if err != nil {
+		log.Errorf("Error while processing 'arping' command: %s", err)
+		dataCh <- ret
+		return
+	}
 	lines := strings.Split(string(bytes), "\n")
 	if len(lines) == 0 {
 		log.Warnf("Missing output for arping")
-		l2chan <- ret
+		dataCh <- ret
 		return
 	}
 
@@ -150,7 +280,7 @@ func runArping(dstAddr string, dstMac string, allDstMacs []string, srcNic string
 	parts := hRgegex.FindStringSubmatch(lines[0])
 	if len(parts) != 4 {
 		log.Warnf("Wrong format for header line: %s", lines[0])
-		l2chan <- ret
+		dataCh <- ret
 		return
 	}
 
@@ -161,148 +291,48 @@ func runArping(dstAddr string, dstMac string, allDstMacs []string, srcNic string
 		if len(parts) != 3 {
 			continue
 		}
-		remoteMac := strings.ToLower(parts[2])
-		ret.RemoteMac = remoteMac
-		ret.Successful = macInDstMacs(remoteMac, allDstMacs)
+		remoteMAC := strings.ToLower(parts[2])
+		ret.RemoteMac = remoteMAC
+		ret.Successful = macInDstMacs(remoteMAC, allDstMACs)
 		if !ret.Successful {
-			log.Warnf("Unexpected mac address for arping %s on nic %s: %s", dstAddr, srcNic, remoteMac)
-		} else if strings.ToLower(dstMac) != remoteMac {
-			log.Infof("Received remote mac %s different then expected mac %s", remoteMac, dstMac)
+			log.Warnf("Unexpected mac address for arping %s on nic %s: %s", dstAddr, srcNIC, remoteMAC)
 		}
-		l2chan <- ret
+		if strings.ToLower(dstMAC) != remoteMAC {
+			log.Infof("Received remote mac %s different then expected mac %s", remoteMAC, dstMAC)
+		}
+		dataCh <- ret
 	}
 }
 
-func analyzeNmap(dstAddr string, dstMac string, allDstMacs []string, srcNic string, l2chan chan Any, output func() ([]byte, error)) {
-
-	ret := &models.L2Connectivity{
-		OutgoingNic:       srcNic,
-		OutgoingIPAddress: "",
-		RemoteIPAddress:   dstAddr,
-		RemoteMac:         "",
-		Successful:        false,
-	}
-
-	out, err := output()
-	if err != nil {
-		log.WithError(err).Warn("nmap command failed")
-		l2chan <- ret
-		return
-	}
-
-	var nmaprun nmap.Nmaprun
-	if err := xml.Unmarshal(out, &nmaprun); err != nil {
-		log.WithError(err).Warn("Failed to un-marshal nmap XML")
-		l2chan <- ret
-		return
-	}
-
-	for _, h := range nmaprun.Hosts {
-
-		if h.Status.State != "up" {
-			continue
-		}
-
-		for _, a := range h.Addresses {
-
-			if a.AddrType != "mac" {
-				continue
-			}
-
-			remoteMac := strings.ToLower(a.Addr)
-			ret.RemoteMac = remoteMac
-			ret.Successful = macInDstMacs(remoteMac, allDstMacs)
-			if !ret.Successful {
-				log.Warnf("Unexpected MAC address for nmap %s on NIC %s: %s", dstAddr, srcNic, remoteMac)
-			} else if strings.ToLower(dstMac) != remoteMac {
-				log.Infof("Received remote MAC %s different then expected MAC %s", remoteMac, dstMac)
-			}
-
-			l2chan <- ret
-			return
-		}
-	}
-
-	l2chan <- ret
-}
-
-func l2CheckAddress(dstAddr string, dstMac string, allDstMacs, sourceNics []string, l2chan chan Any, l2DoneChan chan Any) {
-	defer sendDone(l2DoneChan)
-	innerChan := make(chan Any, 1000)
-	for _, srcNic := range sourceNics {
-		go l2CheckAddressOnNic(dstAddr, dstMac, allDstMacs, srcNic, innerChan)
-	}
-	received := false
-	for numDone := 0; numDone != len(sourceNics); {
-		iret := <-innerChan
-		switch ret := iret.(type) {
-		case *models.L2Connectivity:
-			received = true
-			l2chan <- ret
-		case Done:
-			numDone++
-		}
-	}
-	if !received {
-		ret := &models.L2Connectivity{
-			OutgoingNic:       "",
-			OutgoingIPAddress: "",
-			RemoteIPAddress:   dstAddr,
-			RemoteMac:         "",
-			Successful:        false,
-		}
-		l2chan <- ret
-	}
-}
-
-func l2CheckConnectivity(destinationNics []*models.ConnectivityCheckNic, sourceNics []string, l2chan chan Any) {
-	defer sendDone(l2chan)
-	doneChan := make(chan Any)
-	allDstMacs := make([]string, 0)
-	for _, destNic := range destinationNics {
-		allDstMacs = append(allDstMacs, destNic.Mac)
-	}
-	numAddresses := 0
-	for _, destNic := range destinationNics {
-		for _, address := range destNic.IPAddresses {
-			numAddresses++
-			go l2CheckAddress(address, destNic.Mac, allDstMacs, sourceNics, l2chan, doneChan)
-		}
-	}
-	for i := 0; i != numAddresses; i++ {
-		<-doneChan
-	}
-}
-
-func checkHost(outgoingNics []string, host *models.ConnectivityCheckHost, hostChan chan *models.ConnectivityRemoteHost) {
+func checkHost(conCheck connectivityCmd, outCh chan *models.ConnectivityRemoteHost) {
 	ret := &models.ConnectivityRemoteHost{
-		HostID:         host.HostID,
-		L2Connectivity: make([]*models.L2Connectivity, 0),
-		L3Connectivity: make([]*models.L3Connectivity, 0),
+		HostID:         conCheck.getHost().HostID,
+		L2Connectivity: []*models.L2Connectivity{},
+		L3Connectivity: []*models.L3Connectivity{},
 	}
-	addresses := getOutgoingAddresses(host.Nics)
-	ch := make(chan Any, 1000)
-	go l3CheckConnectivity(addresses, outgoingNics, ch)
-	go l2CheckConnectivity(host.Nics, outgoingNics, ch)
+	addresses := getOutgoingAddresses(conCheck.getHost().Nics)
+	dataCh := make(chan any)
+	go l3CheckConnectivity(addresses, dataCh, conCheck)
+	go l2CheckConnectivity(dataCh, conCheck)
 	for numDone := 0; numDone != 2; {
-		iret := <-ch
-		switch value := iret.(type) {
-		case *models.L2Connectivity:
-			ret.L2Connectivity = append(ret.L2Connectivity, value)
+		v := <-dataCh
+		switch value := v.(type) {
 		case *models.L3Connectivity:
 			ret.L3Connectivity = append(ret.L3Connectivity, value)
-		case Done:
+		case *models.L2Connectivity:
+			ret.L2Connectivity = append(ret.L2Connectivity, value)
+		case done:
 			numDone++
 		}
-	}
-	hostChan <- ret
-}
 
+	}
+	outCh <- ret
+}
 func ConnectivityCheck(_ string, args ...string) (stdout string, stderr string, exitCode int) {
 	if len(args) != 1 {
 		return "", "Expecting exactly 1 argument for connectivity command", -1
 	}
-	params := make(models.ConnectivityCheckParams, 0)
+	params := models.ConnectivityCheckParams{}
 	err := json.Unmarshal([]byte(args[0]), &params)
 	if err != nil {
 		log.Warnf("Error unmarshalling json %s: %s", args[0], err.Error())
@@ -311,9 +341,10 @@ func ConnectivityCheck(_ string, args ...string) (stdout string, stderr string, 
 	nics := getOutgoingNics()
 	hostChan := make(chan *models.ConnectivityRemoteHost)
 	for _, host := range params {
-		go checkHost(nics, host, hostChan)
+		h := hostChecker{outgoingNICS: nics, host: host}
+		go checkHost(h, hostChan)
 	}
-	ret := models.ConnectivityReport{RemoteHosts: make([]*models.ConnectivityRemoteHost, 0)}
+	ret := models.ConnectivityReport{RemoteHosts: []*models.ConnectivityRemoteHost{}}
 	for i := 0; i != len(params); i++ {
 		ret.RemoteHosts = append(ret.RemoteHosts, <-hostChan)
 	}
@@ -323,4 +354,27 @@ func ConnectivityCheck(_ string, args ...string) (stdout string, stderr string, 
 		return "", err.Error(), -1
 	}
 	return string(bytes), "", 0
+}
+
+type hostChecker struct {
+	host         *models.ConnectivityCheckHost
+	outgoingNICS []string
+}
+
+type connectivityCmd interface {
+	command(name string, args []string) ([]byte, error)
+	getHost() *models.ConnectivityCheckHost
+	getOutgoingNICs() []string
+}
+
+func (hc hostChecker) getHost() *models.ConnectivityCheckHost {
+	return hc.host
+}
+
+func (hc hostChecker) getOutgoingNICs() []string {
+	return hc.outgoingNICS
+}
+
+func (hc hostChecker) command(name string, args []string) ([]byte, error) {
+	return exec.Command(name, args...).CombinedOutput()
 }

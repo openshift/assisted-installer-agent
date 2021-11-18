720bfed358fc01f827413c4e95ffc0cfc127b616
Omer Tuchfeld <otuchfel@redhat.com>
2021-11-18 16:33:22 +0100

    MGMT-7941: Allow forcing the IP address and hostname of the agent in dry mode (#256)
    Dry run mode was only tried so far with single-node. This commit makes
    the IP and hostname configurable, allowing the swarm to launch
    multi-node clusters without having hosts clash.
    
    It also disables the connectivity check in dry run mode because with
    different fake IP addresses that would obviously not work

--- src/config/dry_run_config.go
+++ src/config/dry_run_config.go
@@ -12,7 +12,9 @@ import (
 type DryRunConfig struct {
 	DryRunEnabled        bool   `envconfig:"DRY_ENABLE"`
 	ForcedHostID         string `envconfig:"DRY_HOST_ID"`
-	ForcedMacAddress     string `envconfig:"DRY_MAC_ADDRESS"`
+	ForcedHostIPv4       string `envconfig:"DRY_FORCED_HOST_IPV4"`
+	ForcedMacAddress     string `envconfig:"DRY_FORCED_MAC_ADDRESS"`
+	ForcedHostname       string `envconfig:"DRY_FORCED_HOSTNAME"`
 	FakeRebootMarkerPath string `envconfig:"DRY_FAKE_REBOOT_MARKER_PATH"`
 }
 
@@ -21,7 +23,9 @@ var GlobalDryRunConfig DryRunConfig
 var DefaultDryRunConfig DryRunConfig = DryRunConfig{
 	DryRunEnabled:        false,
 	ForcedHostID:         "",
+	ForcedHostIPv4:       "",
 	ForcedMacAddress:     "",
+	ForcedHostname:       "",
 	FakeRebootMarkerPath: "",
 }
 
@@ -35,6 +39,8 @@ func ProcessDryRunArgs() {
 	flag.BoolVar(&GlobalDryRunConfig.DryRunEnabled, "dry-run", DefaultDryRunConfig.DryRunEnabled, "Dry run avoids/fakes certain actions while communicating with the service")
 	flag.StringVar(&GlobalDryRunConfig.ForcedHostID, "force-id", DefaultDryRunConfig.ForcedHostID, "The fake host ID to give to the host")
 	flag.StringVar(&GlobalDryRunConfig.ForcedMacAddress, "force-mac", DefaultDryRunConfig.ForcedMacAddress, "The fake mac address to give to the first network interface")
+	flag.StringVar(&GlobalDryRunConfig.ForcedHostname, "force-hostname", DefaultDryRunConfig.ForcedHostname, "The fake hostname to give to this host")
+	flag.StringVar(&GlobalDryRunConfig.ForcedHostIPv4, "forced-ipv4", DefaultDryRunConfig.ForcedHostIPv4, "The fake ip address to give to the host's network interface")
 	flag.StringVar(&GlobalDryRunConfig.FakeRebootMarkerPath, "fake-reboot-marker-path", DefaultDryRunConfig.FakeRebootMarkerPath, "A path whose existence indicates a fake reboot happened")
 	flag.Parse()
 }

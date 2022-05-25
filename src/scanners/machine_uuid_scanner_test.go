ff1f8bf0bd58ce4a35559abab7ecfa55483bb1d6
CrystalChun <cchun@redhat.com>
2022-05-25 14:29:47 -0700

    MGMT-9175: Pass all interfaces to the service
    https://issues.redhat.com/browse/MGMT-9175
    Based on epic https://issues.redhat.com/browse/MGMT-10087
    This change removes filtering interfaces and sends
    all interfaces to the assisted-service along with the type
    of interface it is.

--- src/scanners/machine_uuid_scanner_test.go
+++ src/scanners/machine_uuid_scanner_test.go
@@ -81,8 +81,8 @@ var _ = Describe("Machine uuid test", func() {
 
 		It(fmt.Sprintf("mac address fallback %s", test.useCase), func() {
 			rets := []agentutils.Interface{
-				agentutils.NewFilledInterfaceMock(1500, "eth0", "f8:75:a4:a4:00:fe", net.FlagBroadcast|net.FlagUp, []string{"10.0.0.18/24", "192.168.6.7/20", "fe80::d832:8def:dd51:3527/128", "de90::d832:8def:dd51:3527/128"}, true, false, false, 100),
-				agentutils.NewFilledInterfaceMock(1400, "eth1", "f8:75:a4:a4:00:ff", net.FlagBroadcast|net.FlagLoopback, []string{"10.0.0.19/24", "192.168.6.8/20", "fe80::d832:8def:dd51:3528/127", "de90::d832:8def:dd51:3528/127"}, true, false, false, 10),
+				agentutils.NewFilledInterfaceMock(1500, "eth0", "f8:75:a4:a4:00:fe", net.FlagBroadcast|net.FlagUp, []string{"10.0.0.18/24", "192.168.6.7/20", "fe80::d832:8def:dd51:3527/128", "de90::d832:8def:dd51:3527/128"}, true, false, false, 100, "physical"),
+				agentutils.NewFilledInterfaceMock(1400, "eth1", "f8:75:a4:a4:00:ff", net.FlagBroadcast|net.FlagLoopback, []string{"10.0.0.19/24", "192.168.6.8/20", "fe80::d832:8def:dd51:3528/127", "de90::d832:8def:dd51:3528/127"}, true, false, false, 10, "physical"),
 			}
 			dependencies.On("Interfaces").Return(rets, nil).Once()
 			dependencies.On("Execute", "biosdevname", "-i", "eth0").Return("em2", "", 0).Once()

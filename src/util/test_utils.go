95558b67b28280381ec6ed7e7e5515bd11efa386
Michael Levy <milevy@redhat.com>
2022-06-19 13:56:32 +0300

    NO-ISSUE: fix mock generation. (#380)
    go generate command failed when the package uses the generated code
    The make generate target deletes the old mocks before regenerating them
    mocks.go file used util.MockInterface type so after the deletion the compilation failed which fails the mock generation for this package
    Using mock.Mock embedded object instead

---
+++ src/util/test_utils.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"github.com/stretchr/testify/mock"
+	"net"
+)
+
+func FillInterfaceMock(mock *mock.Mock,mtu int, name string, macAddr string, flags net.Flags, addrs []string, speedMbps int64, interfaceType string) {
+	mock.On("Name").Return(name)
+	mock.On("MTU").Return(mtu)
+	hwAddr, _ := net.ParseMAC(macAddr)
+	mock.On("HardwareAddr").Return(hwAddr)
+	mock.On("Flags").Return(flags)
+	mock.On("Addrs").Return(parseAddresses(addrs), nil).Once()
+	mock.On("SpeedMbps").Return(speedMbps)
+	mock.On("Type").Return(interfaceType, nil).Once()
+}
+
+func parseAddresses(addrs []string) []net.Addr {
+	ret := make([]net.Addr, 0)
+	for _, a := range addrs {
+		ret = append(ret, parseAddress(a))
+	}
+	return ret
+}
+
+func parseAddress(addrStr string) net.Addr {
+	ip, ipnet, err := net.ParseCIDR(addrStr)
+	if err != nil {
+		return &net.IPNet{}
+	}
+	return &net.IPNet{IP: ip, Mask: ipnet.Mask}
+}

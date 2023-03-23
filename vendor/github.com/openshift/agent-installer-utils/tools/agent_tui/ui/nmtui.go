835acad181d540a40a4622c94b8faa3ad06bd113
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-03-23 17:27:22 +0000

    OCPBUGS-8695: vendor agent-installer-utils to v0.0.0-20230310220517-8506fbb6a346 (#518)
    Co-authored-by: Andrea Fasano <afasano@redhat.com>

--- vendor/github.com/openshift/agent-installer-utils/tools/agent_tui/ui/nmtui.go
+++ vendor/github.com/openshift/agent-installer-utils/tools/agent_tui/ui/nmtui.go
@@ -6,48 +6,41 @@ import (
 	"os/exec"
 
 	"github.com/nmstate/nmstate/rust/src/go/nmstate/v2"
-	"github.com/openshift/agent-installer-utils/tools/agent_tui/dialogs"
 	"github.com/openshift/agent-installer-utils/tools/agent_tui/net"
-	"github.com/rivo/tview"
 )
 
-func (u *UI) ShowNMTUI(treeView *tview.TreeView) {
+func (u *UI) ShowNMTUI() error {
 	u.nmtuiActive.Store(true)
 	defer u.nmtuiActive.Store(false)
 
+	var nmtuiErr error
 	u.app.Suspend(func() {
 		cmd := exec.Command("nmtui")
 		cmd.Stdin = os.Stdin
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err != nil {
-			dialogs.PanicDialog(u.app, err)
-		}
+		nmtuiErr = cmd.Run()
 	})
+	if nmtuiErr != nil {
+		return nmtuiErr
+	}
+
 	nm := nmstate.New()
 	state, err := nm.RetrieveNetState()
 	if err != nil {
-		dialogs.PanicDialog(u.app, err)
+		return err
 	}
 
 	var netState net.NetState
 	if err := json.Unmarshal([]byte(state), &netState); err != nil {
-		dialogs.PanicDialog(u.app, err)
+		return err
 	}
 
-	//netStatePage, err := modalNetStateJSONPage(&netState, pages)
-	if treeView == nil {
-		netStatePage, err := u.ModalTreeView(netState)
-		if err != nil {
-			dialogs.PanicDialog(u.app, err)
-		}
-		u.pages.AddPage("netstate", netStatePage, true, true)
-	} else {
-		updatedTreeView, err := u.TreeView(netState)
-		if err != nil {
-			dialogs.PanicDialog(u.app, err)
-		}
-		treeView.SetRoot(updatedTreeView.GetRoot())
+	netStatePage, err := u.ModalTreeView(netState)
+	if err != nil {
+		return err
 	}
+	u.pages.AddPage("netstate", netStatePage, true, true)
+
+	return nil
 }

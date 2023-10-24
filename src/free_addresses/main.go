7213119e7de51e76cc4cfb6c8b95b886a9ece5e7
Omer Tuchfeld <omer@tuchfeld.dev>
2023-10-24 14:51:53 +0200

    MGMT-16011: Reduce agent image size (#617)
    * All agent binaries are now simply symlinks to `/usr/bin/agent`, which
      will check argv[0] to know which one it's supposed to emulate.
    
    * Combine all `dnf install` `Dockerfile` steps into one, so that cache
      doesn't take space in the layers that forgot to do cache cleaning
    
    * Cleanup some unused large files after package installations:
    
    ```
    # Clean unnecessary nmap files to reduce image size
    && find /usr/share/nmap/ -mindepth 1 -maxdepth 1 | grep -v nmap-payloads | xargs rm -rf \
    # Remove cracklib installed as part of systemd to reduce image size
    && rm -rf /usr/share/cracklib \
    # Remove manpages && docs to reduce image size
    && rm -rf /usr/share/man /usr/share/doc \
    # Remove RPM/DNF files to reduce image size
    && rm -rf /var/lib/rpm/rpmdb.sqlite /var/lib/dnf
    ```

--- src/free_addresses/main.go
+++ src/free_addresses/main.go
@@ -1,4 +1,4 @@
-package main
+package free_addresses
 
 import (
 	"flag"
@@ -8,11 +8,10 @@ import (
 	"github.com/openshift/assisted-installer-agent/src/config"
 	"github.com/openshift/assisted-installer-agent/src/util"
 
-	"github.com/openshift/assisted-installer-agent/src/free_addresses"
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
+func Main() {
 	subprocessConfig := config.ProcessSubprocessArgs(config.DefaultLoggingConfig)
 	config.ProcessDryRunArgs(&subprocessConfig.DryRunConfig)
 	util.SetLogging("free_addresses", subprocessConfig.TextLogging, subprocessConfig.JournalLogging, subprocessConfig.StdoutLogging, subprocessConfig.ForcedHostID)
@@ -20,7 +19,7 @@ func main() {
 		log.Warnf("Expecting exactly single argument to free_addresses. Received %d", len(os.Args)-1)
 		os.Exit(-1)
 	}
-	stdout, stderr, exitCode := free_addresses.GetFreeAddresses(flag.Arg(0), &free_addresses.ProcessExecuter{}, log.StandardLogger())
+	stdout, stderr, exitCode := GetFreeAddresses(flag.Arg(0), &ProcessExecuter{}, log.StandardLogger())
 	fmt.Fprint(os.Stdout, stdout)
 	fmt.Fprint(os.Stderr, stderr)
 	os.Exit(exitCode)

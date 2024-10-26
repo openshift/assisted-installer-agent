d608d9b58f9d08ce248251fb087e3def8610e1e3
DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>
2024-10-26 11:18:13 +0530

    ACM-15078: Updated paramaters for zipl for s390x
    Signed-off-by: DAMISETTI-VEERABHADRARAO <damisetti.veerabhadrarao@ibm.com>

--- src/commands/actions/reboot_for_reclaim.go
+++ src/commands/actions/reboot_for_reclaim.go
@@ -3,7 +3,9 @@ package actions
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
 	"runtime"
+	"strings"
 	"syscall"
 
 	"github.com/openshift/assisted-installer-agent/src/util"
@@ -29,21 +31,64 @@ func (a *rebootForReclaim) Run() (stdout, stderr string, exitCode int) {
 	}
 
 	if runtime.GOARCH == "s390x" {
+		var options string
+		cmdline_params := make(map[string]string)
+		var requiredCmdline string
+
+		stdout, stderr, exitCode = util.Execute("cat", "/boot/loader/entries/00-assisted-discovery.conf")
+		if exitCode != 0 {
+			return stdout, stderr, exitCode
+		}
+		lines := strings.Split(stdout, "\n")
+		for _, line := range lines {
+			if strings.HasPrefix(line, "options") {
+				options = strings.TrimSpace(strings.TrimPrefix(line, "options"))
+				break
+			}
+		}
+		stdout, stderr, exitCode := util.Execute("cat", "/proc/cmdline")
+		if exitCode != 0 {
+			return stdout, stderr, exitCode
+		}
+
+		// Parameters to extract from cmdline for agents
+		paramsToExtract := []string{
+			"ip",
+			"nameserver",
+			"rd.znet",
+			"zfcp.allow_lun_scan",
+			"rd.zfcp",
+			"rd.dasd",
+		}
+
+		for _, param := range paramsToExtract {
+			regex := regexp.MustCompile(fmt.Sprintf(`\b%s=([^\s]+)`, param))
+			match := regex.FindStringSubmatch(stdout)
+			if len(match) > 1 {
+				cmdline_params[param] = match[1]
+			}
+		}
+
+		for key, value := range cmdline_params {
+			requiredCmdline += fmt.Sprintf("%s=%s ", key, value)
+		}
+
 		unshareCommand := "unshare"
 		unshareArgs := []string{
 			"--mount",
 			"bash",
 			"-c",
-			fmt.Sprintf("mount -o remount,rw /boot && zipl -V -t /boot -i %s/%s -r %s/%s -c /boot/loader/entries%s -p /boot/loader/entries%s",
+			fmt.Sprintf("mount -o remount,rw /boot && zipl -V -t /boot -i %s/%s -r %s/%s -P '%s %s'",
 				artifactsFolder, kernelFile,
 				artifactsFolder, initrdFile,
-				bootLoaderConfigFileName,
-				bootLoaderConfigFileName),
+				options,
+				requiredCmdline),
 		}
 		stdout, stderr, exitCode = util.Execute(unshareCommand, unshareArgs...)
 		if exitCode != 0 {
 			return stdout, stderr, exitCode
 		}
+
 	}
 	return util.Execute("systemctl", "reboot")
 }
